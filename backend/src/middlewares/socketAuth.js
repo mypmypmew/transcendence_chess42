92232d3c3ec5e7b02cd6ab5c1fadc0a79be5c339
Serhii Nazarov <mypmypmew.dev@gmail.com>
2026-08-24 17:30:17 +0200

    fix(backend): parse Socket.IO session cookies correctly

--- backend/src/middlewares/socketAuth.js
+++ backend/src/middlewares/socketAuth.js
@@ -1,25 +1,26 @@
-    const { parseCookie } = require('cookie'); 
-    const sessionService = require('../services/sessionService');
+const { parse } = require('cookie');
+const sessionService = require('../services/sessionService');
 
-    async function socketAuth(socket, next) {
-    try {
-        const header = socket.request.headers.cookie || '';
-        const cookies = parseCookie(header); 
 
-        const session = await sessionService.getSession({ cookies });
+async function socketAuth(socket, next) {
+  try {
+    const header = socket.request.headers.cookie || '';
+    const cookies = parse(header);
 
-        if (!session) {
-        return next(new Error('Unauthorized'));
-        }
+    const session = await sessionService.getSession({ cookies });
 
-        socket.data.userId = session.userId;
-        socket.data.user = session.user;
-
-        next();
-    } catch (err) {
-        console.error('socket auth error:', err);
-        next(new Error('Unauthorized'));
+    if (!session) {
+      return next(new Error('Unauthorized'));
     }
-    }
+
+    socket.data.userId = session.userId;
+    socket.data.user = session.user;
+
+    next();
+  } catch (err) {
+    console.error('socket auth error:', err);
+    next(new Error('Unauthorized'));
+  }
+}
 
     module.exports = socketAuth;
