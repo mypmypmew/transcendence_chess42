58490f6257fca528e1c7e1ff3219738fbb055bbb
Serhii Nazarov <mypmypmew.dev@gmail.com>
2026-08-24 17:36:33 +0200

    Revert "fix(backend): parse Socket.IO session cookies correctly"
    This reverts commit 92232d3c3ec5e7b02cd6ab5c1fadc0a79be5c339.

--- backend/src/middlewares/socketAuth.js
+++ backend/src/middlewares/socketAuth.js
@@ -1,26 +1,25 @@
-const { parse } = require('cookie');
-const sessionService = require('../services/sessionService');
+    const { parseCookie } = require('cookie'); 
+    const sessionService = require('../services/sessionService');
 
+    async function socketAuth(socket, next) {
+    try {
+        const header = socket.request.headers.cookie || '';
+        const cookies = parseCookie(header); 
 
-async function socketAuth(socket, next) {
-  try {
-    const header = socket.request.headers.cookie || '';
-    const cookies = parse(header);
+        const session = await sessionService.getSession({ cookies });
 
-    const session = await sessionService.getSession({ cookies });
+        if (!session) {
+        return next(new Error('Unauthorized'));
+        }
 
-    if (!session) {
-      return next(new Error('Unauthorized'));
+        socket.data.userId = session.userId;
+        socket.data.user = session.user;
+
+        next();
+    } catch (err) {
+        console.error('socket auth error:', err);
+        next(new Error('Unauthorized'));
     }
-
-    socket.data.userId = session.userId;
-    socket.data.user = session.user;
-
-    next();
-  } catch (err) {
-    console.error('socket auth error:', err);
-    next(new Error('Unauthorized'));
-  }
-}
+    }
 
     module.exports = socketAuth;
