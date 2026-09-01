12eb2868fd7a21b1111813c6084c80610f29fe0a
tndreka <tndreka@student.42heilbronn.de>
2026-09-01 20:24:28 +0200

    feat(backend): added imports and error function

---
+++ backend/src/services/chatService.js
@@ -0,0 +1,7 @@
+const chatRepository = require('../repositories/chatRepository');
+
+function httpError(status, message) {
+  const err = new Error(message);
+  err.status = status;
+  return err;
+}
