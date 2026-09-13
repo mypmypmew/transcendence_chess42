9be54d9a4737681baa33a6948b0c3b6d9deff83d
Serhii Nazarov <mypmypmew.dev@gmail.com>
2026-09-13 22:56:59 +0200

    refactor(frontend): route API and Socket.IO through same origin

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,8 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000'
-
-const socket = io(SOCKET_URL, {
+const socket = io(window.location.origin, {
   autoConnect: false,
   withCredentials: true,
 })
